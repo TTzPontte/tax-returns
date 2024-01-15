73863964b2f3e5ad68312e31eb24b7bed9d2b8f3
Matheus dos Santos <matheus.santos@pontte.com.br>
2024-01-15 14:32:42 -0300

    feat: pdf staging and dev enviroments

--- src/TaxReturnsCreatePDF/index.js
+++ src/TaxReturnsCreatePDF/index.js
@@ -4,8 +4,32 @@ const { generateTaxReturnPdf } = require("./PDF");
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context) => {
-  const parsedEvent = event
-  const [{ contractInfo, participants, receiverInfo, installments }] = parsedEvent.data;
+  const parsedEvent = event;
+  const [{ contractInfo, participants, receiverInfo, installments }] =
+    parsedEvent.data;
+
+  // Verificar se a variável TABLE_NAME contém "DEV", "STAGING" ou "PROD"
+  const environment = process.env.TABLE_NAME.includes("dev")
+    ? "dev"
+    : process.env.TABLE_NAME.includes("staging")
+    ? "staging"
+    : process.env.TABLE_NAME.includes("prod")
+    ? "prod"
+    : (() => {
+        console.error("Nome da tabela não contém DEV, STAGING ou PROD.");
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            error: "Nome da tabela não contém DEV, STAGING ou PROD.",
+          }),
+        };
+      })();
+
+  if (environment instanceof Object) {
+    // Se o ambiente não for encontrado, retorne o erro
+    return environment;
+  }
+
   try {
     const pdfBuffer = await generateTaxReturnPdf({
       contractInfo,
@@ -18,20 +42,33 @@ exports.handler = async (event, context) => {
 
     const s3FileName = `${contractInfo.development}.pdf`;
 
+    const s3Bucket =
+      environment === "dev"
+        ? "taxreturns-frontend-storage-73711795155840-dev"
+        : environment === "staging"
+        ? "taxreturns-frontend-storage-73711795134329-staging"
+        : environment === "prod"
+        ? ""
+        : "";
+
+        console.log(s3Bucket)
+
     const s3Params = {
-      Bucket: "taxreturns-frontend-storage-73711795134329-staging",
-      Key: "/public/IR2023/" + s3FileName,
+      Bucket: s3Bucket,
+      Key: `public/IR2023/${s3FileName}`,
       Body: pdfBuffer,
       ContentType: "application/pdf",
     };
-    
+
     await s3.upload(s3Params).promise();
 
-    console.log(`PDF salvo com sucesso no S3: ${s3Params.Bucket}/${s3Params.Key}`);
+    console.log(
+      `PDF salvo com sucesso no S3: ${s3Params.Bucket}/${s3Params.Key}`
+    );
 
     return {
       statusCode: 200,
-      body: "deu",
+      body: "Success",
     };
   } catch (pdfBufferError) {
     console.error({ pdfBufferError });
