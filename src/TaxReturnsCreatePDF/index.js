bc03d42e792f448cfc8a60b1a3dd068cf2fa692a
Matheus dos Santos <matheus.santos@pontte.com.br>
2024-01-17 17:09:31 -0300

    refactor: index refactor

--- src/TaxReturnsCreatePDF/index.js
+++ src/TaxReturnsCreatePDF/index.js
@@ -4,67 +4,51 @@ const { generateTaxReturnPdf } = require("./PDF");
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context) => {
-  const parsedEvent = event;
-  
-  const {
-    data: {
-      getContractInfo: {
-        total,
-        email,
-        development,
-        date,
-        contractNumber,
-        block,
-        baseYear,
-        balance,
-        unit,
-        Installments: { items: installments },
-        Participants: { items: participants },
-      },
-    },
-  } = parsedEvent;
-  
-  const contractInfo = {
-    total,
-    email,
-    development,
-    date,
-    contractNumber,
-    block,
-    baseYear,
-    balance,
-    unit
-  };
-
-  const receiverInfo ={
-    "receiver": "MAUÁ CAPITAL REAL ESTATE DEBT III | Investment Multimarket Fund",
-    "cnpj": "30.982.547/0001-09",
-    "address": "AV BRIGADEIRO FARIA LIMA, 2277 - SÃO PAULO",
-    "date": "SÃO PAULO, 05 DE FEVEREIRO DE 2021"
-  }  
-
-  // Verificar se a variável TABLE_NAME contém "DEV", "STAGING" ou "PROD"
-  const environment = process.env.TABLE_NAME.includes("dev")
-    ? "dev"
-    : process.env.TABLE_NAME.includes("staging")
-    ? "staging"
-    : process.env.TABLE_NAME.includes("prod")
-    ? "prod"
-    : (() => {
-        console.error("Nome da tabela não contém DEV, STAGING ou PROD.");
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            error: "Nome da tabela não contém DEV, STAGING ou PROD.",
-          }),
-        };
-      })();
-
-  if (environment instanceof Object) {
-    return environment;
-  }
-
   try {
+    const {
+      data: {
+        getContractInfo: {
+          total,
+          email,
+          development,
+          date,
+          contractNumber,
+          block,
+          baseYear,
+          balance,
+          unit,
+          Installments: { items: installments },
+          Participants: { items: participants },
+        },
+      },
+    } = event;
+
+    const contractInfo = {
+      total,
+      email,
+      development,
+      date,
+      contractNumber,
+      block,
+      baseYear,
+      balance,
+      unit,
+    };
+
+    const receiverInfo = {
+      receiver: "MAUÁ CAPITAL REAL ESTATE DEBT III | Investment Multimarket Fund",
+      cnpj: "30.982.547/0001-09",
+      address: "AV BRIGADEIRO FARIA LIMA, 2277 - SÃO PAULO",
+      date: "SÃO PAULO, 05 DE FEVEREIRO DE 2021",
+    };
+
+    const environments = ["dev", "staging", "prod"];
+    const environment = environments.find(env => process.env.TABLE_NAME.includes(env));
+
+    if (!environment) {
+      throw new Error("Nome da tabela não contém DEV, STAGING ou PROD.");
+    }
+
     const pdfBuffer = await generateTaxReturnPdf({
       contractInfo,
       proposal: {
@@ -84,7 +68,6 @@ exports.handler = async (event, context) => {
         : environment === "prod"
         ? ""
         : "";
-
     const s3Params = {
       Bucket: s3Bucket,
       Key: `public/IR2023/${s3FileName}`,
@@ -94,16 +77,14 @@ exports.handler = async (event, context) => {
 
     await s3.upload(s3Params).promise();
 
-    console.log(
-      `PDF salvo com sucesso no S3: ${s3Params.Bucket}/${s3Params.Key}`
-    );
+    console.log(`PDF salvo com sucesso no S3: ${s3Params.Bucket}/${s3Params.Key}`);
 
     return {
       statusCode: 200,
       body: "Success",
     };
-  } catch (pdfBufferError) {
-    console.error({ pdfBufferError });
+  } catch (error) {
+    console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Erro ao gerar ou salvar o PDF" }),
