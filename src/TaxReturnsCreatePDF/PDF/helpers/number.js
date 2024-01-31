ca3414124484af3d5601bae3e9e599fd1724890d
Matheus dos Santos <matheus.santos@pontte.com.br>
2024-01-31 18:47:10 -0300

    fix: numbers

--- src/TaxReturnsCreatePDF/PDF/helpers/number.js
+++ src/TaxReturnsCreatePDF/PDF/helpers/number.js
@@ -2,25 +2,31 @@ const formatMoney = (value = 0) =>
   value
     ? `R$ ${Number(value)
         .toFixed(2)
-        .replace('.', ',')
-        .replace(/\d(?=(\d{3})+,)/g, '$&.')}`
-    : 'R$ 0,00';
+        .replace(".", ",")
+        .replace(/\d(?=(\d{3})+,)/g, "$&.")}`
+    : "R$ 0,00";
 
-const formatPercentage = (value = 0) =>
+const formatPercentage = (value) => {
+  const formattedValue = Number(value).toFixed(2).replace(".", ",");
+  return formattedValue === "0,00" ? "0%" : `${formattedValue}%`;
+};
+
+const removeFormatMoney = (value) =>
+  typeof value !== "string"
+    ? value
+    : parseFloat(value.replace(/[R$ .]/g, "").replace(",", "."));
+
+const formatMoneyWOCurrency = (value) =>
   value
     ? `${Number(value)
         .toFixed(2)
-        .replace('.', ',')}%`
-    : '0%';
+        .replace(".", ",")
+        .replace(/\d(?=(\d{3})+,)/g, "$&.")}`
+    : "0,00";
 
-const removeFormatMoney = value => (typeof value !== 'string' ? value : parseFloat(value.replace(/[R$ .]/g, '').replace(',', '.')));
-
-const formatMoneyWOCurrency = value =>
-  value
-    ? `${Number(value)
-        .toFixed(2)
-        .replace('.', ',')
-        .replace(/\d(?=(\d{3})+,)/g, '$&.')}`
-    : '0,00';
-
-module.exports = { formatPercentage, removeFormatMoney, formatMoneyWOCurrency, formatMoney };
+module.exports = {
+  formatPercentage,
+  removeFormatMoney,
+  formatMoneyWOCurrency,
+  formatMoney,
+};
